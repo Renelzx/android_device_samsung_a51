230ad6cd39bf1ffc012511fe61be4c2bd6a68dd8
Renelz <131983767+Renelzx@users.noreply.github.com>
2026-01-19 10:49:35 +0700

    Update lineage_a51.mk

--- lineage_a51.mk
+++ lineage_a51.mk
@@ -15,7 +15,18 @@
 #
 
 # FOD
-TARGET_HAS_FOD := true
+#TARGET_HAS_FOD := true
+
+# Inherit some common device props
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_DISABLE_EPPE := true
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/a51/device.mk)
